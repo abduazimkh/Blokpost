6103ebd801b2afd5749fdb00cfb4f273ce0ab770
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-09 13:50:29 +0500

    nav and routercontroller added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './scss/App.scss'
+import Navbar from './layout/navbar/Navbar';
+import RouteController from './routes';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <RouteController/>
+    </>
+  );
+}
+
+export default App;

323fd5661b0284a35d0ff244029d513e662d505e
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-09 14:15:40 +0500

    Routes

---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/home/Home";
+import Article from "../pages/article/Article";
+import Admin from "../pages/admin/Admin";
+
+import Create from "./sub-routes/create/Create";
+import Articles from "./sub-routes/articles/Articles";
+import { Users } from "./sub-routes/users/Users";
+import Auth from "../pages/auth/Auth";
+import Login from "./sub-routes/login/Login";
+import Signup from "./sub-routes/signup/Signup";
+
+const RouteController = ()=>{
+    return(
+        <Routes>
+            <Route path='' element={<Home/>}/>
+            <Route path='article/:id' element={<Article/>}/>
+            <Route path='auth' element={<Auth/>}>
+                <Route path='login' element={<Login/>}/>
+                <Route path='signup' element={<Signup/>}/>
+            </Route>
+            <Route path='admin' element={<Admin/>}>
+                <Route path='create' element={<Create/>}/>
+                <Route path='articles' element={<Articles/>}/>
+                <Route path='users' element={<Users/>}/>
+            </Route>
+        </Routes>
+    )
+}
+export default RouteController

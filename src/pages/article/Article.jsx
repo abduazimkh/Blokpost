406b42ef9263c2a4c4f9d564ed4c3f24fddf4a1e
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:44:00 +0500

    single article added

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import instance from '../../services/api';
+import { Container } from '../../utils';
+import "./Article.scss";
 
 const Article = () => {
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    instance(`/api/posts/${id}`)
+    .then(res => setData(res.data))
+  }, [])
+
+  console.log(data.title);
+
   return (
-    <div>Article</div>
+    <Container>
+      {
+        <div className='single-article'>
+          <h2>{data.title}</h2>
+          <img src={data.image} alt="picture" />
+          <p>{data.title}</p>
+
+          <p>{data.description}</p>
+        </div>
+      }
+    </Container>
   )
 }
 

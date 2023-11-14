250b9c6398f55dde865f6aae7791f20c2fbe61af
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:40:54 +0500

    provider added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import './index.scss';
 import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
-const App = lazy(()=> import('./App'))
+import './index.scss';
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from './context/AppProvider';
+const App = lazy(() => import('./App'));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+  <BrowserRouter>
     <Suspense fallback={<p>Loading...</p>}>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </Suspense>
-    </BrowserRouter>
+  </BrowserRouter>
 );
+
+// TODO ROUTER DOM
+// TODO AXIOS
+// TODO FOLDER STRUCTURE
+// TODO ROOT

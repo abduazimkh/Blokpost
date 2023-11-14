67c799f5f0c68e701d39a30f15658639ea153c7c
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:33:55 +0500

    create sub route added

--- src/routes/sub-routes/create/Create.jsx
+++ src/routes/sub-routes/create/Create.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+
 
 const Create = () => {
+  const [value, setValue] = useState(''); 
   return (
-    <div>Create</div>
+    <div>
+      <form >
+        <input type="text" placeholder='Title'/>
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+
+        <select name="" id="">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </form>
+    </div>
   )
 }
 

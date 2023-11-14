cd42cde0245869611cab3954f77a0f42a1a6b386
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:31:30 +0500

    sidebar added

--- src/layout/sidebar/Sidebar.jsx
+++ src/layout/sidebar/Sidebar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import './Sidebar.scss';
+import logo from "../../assets/images/logo.svg";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <Link to="/" className='sidebar__logo'>
+        <img src={logo} alt="picture" />
+      </Link>
+
+      <ul className='sidebar__list'>
+        <li>
+          <NavLink end className={({isActive}) => isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"} to="/admin">
+            Main
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({isActive}) => isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"} to="/admin/create">
+            Create
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={({isActive}) => isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"} to="/admin/articles">
+            Article
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   )
 }
 

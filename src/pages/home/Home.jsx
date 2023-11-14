f6f932c72a4a037353b59fa1e233145af2a75dc4
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:44:56 +0500

    home page added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Button } from '../../utils'
 import Hero from '../../components/hero/Hero'
 import Reel from '../../components/reel/Reel'
 
 const Home = () => {
   return (
     <div>
-        <Hero/>
-        <Reel/>
+      <Hero/>
+      <Reel/>
     </div>
   )
 }

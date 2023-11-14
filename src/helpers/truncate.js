0a5234874519bdf20d09d803eb0e74346372befa
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:29:53 +0500

    tepleate suffix

--- src/helpers/truncate.js
+++ src/helpers/truncate.js
@@ -1,3 +1,4 @@
-const truncate = (string, max, suffix) => string.length > max ? string.split('').slice(0, max).join('') + suffix : string
+const truncate = (string, max, suffix) => string.length > max ?
+string.split("").slice(0, max).join("") + suffix : string
 
 export {truncate}

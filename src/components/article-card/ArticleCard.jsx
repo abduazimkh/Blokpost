5551df8ae4b18abd5c7d4248687d09106e863791
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:37:10 +0500

    article card component added

--- src/components/article-card/ArticleCard.jsx
+++ src/components/article-card/ArticleCard.jsx
@@ -1,19 +1,23 @@
-import './ArticleCard.scss'
-import { truncate } from '../../helpers/truncate'
+import "./ArticleCard.scss";
+import { truncate } from "../../helpers/truncate";
+import { Link } from 'react-router-dom';
 
-const ArticleCard = ({image, title, description, author, createdAt, category}) => {
+const ArticleCard = ({ id, image, title, description, author, createdAt, category}) => {
+
   return (
-    <div className='article__card'>
-        <div className='article__card-image'>
-            <img src={image} alt="" />
-        </div>
-        <h3>{truncate(title, 35, '...' )}</h3>
-        <p>{truncate(description, 30, '...')}</p>
-        <div>
-            <p>{author}</p>
-            <p>{createdAt}</p>
-            <p>{category}</p>
-        </div>
+    <div  className='article-card'>
+      <div className='article__card-image'>
+        <Link to={`/article/${id}`}>
+          <img src={image} alt="" />
+        </Link>
+      </div>
+      <h3>{truncate(title, 35, "...")}</h3>
+      <p>{truncate(description, 50, "...")}</p>
+      <div>
+        <p>{author}</p>
+        <p>{createdAt}</p>
+        <p>{category}</p>
+      </div>
     </div>
   )
 }

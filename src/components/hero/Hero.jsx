46447817bdb0b7ea4c1d6446fe21d48a67f68f9d
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:37:28 +0500

    hero section added

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,23 @@
-import { Button, Container } from '../../utils'
-import hero from '../../assets/image/hero.svg'
-import './Hero.scss'
+import React from 'react';
+import "./Hero.scss";
+import hero from "../../assets/images/hero.svg";
+import { Button, Container } from "../../utils";
 
 const Hero = () => {
   return (
     <div className='hero'>
-      <Container>
-        <div className="hero__wrapper">
-          <div className="hero__content">
-        <h2>Stay curios.</h2>
-        <p>Discover stories, thinking, and expertise from writers on any topic</p>
-        <Button type={'large'} text={'Get started'}/>
-      </div>
-        <div className='hero__image'>
-          <img src={hero} alt="" />
-        </div>
-        </div>
-      </Container>
+        <Container>
+          <div className='hero__wrapper'>
+            <div className="hero__content">
+              <h2>Stay curious.</h2>
+              <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+              <Button type={"large"} text="Start Reading" />
+            </div>
+            <div className='hero__image'>
+              <img src={hero} alt="" />
+            </div>
+          </div>
+        </Container>
     </div>
   )
 }

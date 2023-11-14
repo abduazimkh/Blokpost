6067e131b27e51c1977cb9ddb3f67c538d210ea4
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:38:58 +0500

    container button card skeleton components added

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,24 +1,36 @@
-import './Index.scss'
+import "./index.scss";
+import Skeleton from "react-loading-skeleton";
 
-const Button = ({ text, icon, loading, click, type, disabled }) => {
-    return (
-        <button
-            className='btn'
-            disabled={disabled || loading}
-            onClick={click}
-            type={type}
-        >
-            {text}{icon}
-        </button>
-    )
+
+const Button = ({text,icon, loading, click, type, disabled}) => {
+  return (
+    <button className='btn' disabled={disabled || loading} onClick={click} type={type}>
+        {text} {icon}
+    </button>
+  )
 }
 
-const Container = ({children}) =>{
-    return(
-        <div className='container'>
-            {children}
-        </div>
-    )
+const Container = ({children}) => {
+  return (
+    <div className="container">
+      {children}
+    </div>
+  )
 }
 
-export { Button, Container }
+
+const CardSkeleton = ({ amount }) => {
+  return new Array(amount).fill("").map((skeleton, index) => (
+    <div className="article-card" key={index}>
+      <div className="article__card-image">
+        <Skeleton height={250}/>
+      </div>
+      <Skeleton count={3} style={{ marginTop: "10px" }} />
+      <Skeleton count={3} style={{ marginTop: "10px" }} />
+      <Skeleton height={40} style={{marginTop: "22px"}} />
+    </div>
+  ));
+};
+
+
+export { Button, Container, CardSkeleton }

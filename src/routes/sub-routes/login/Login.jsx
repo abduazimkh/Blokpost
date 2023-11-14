2b7150764a0da3406a4838366856dad8177caaf1
abduazimkh <azimjon8253@gmail.com>
2023-11-14 23:34:28 +0500

    login route added

--- src/routes/sub-routes/login/Login.jsx
+++ src/routes/sub-routes/login/Login.jsx
@@ -1,24 +1,40 @@
-import { useState } from "react"
+import {useState} from 'react'
 import instance from "../../../services/api"
+import { useValue } from '../../../context/AppProvider'
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [state, dispatch] = useValue();
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const handleUSerLogin = (e) => {
+
+  const hanldeUserLogin = (e) => {
     e.preventDefault()
+
     instance.post('/api/auth/login', {
       email,
       password
     })
-    .then(data => console.log(data))
+    .then(response => {
+      if(response.data.token){
+        const userdata = {
+          user: response.data.data,
+          token: response.data.token
+        }
+        dispatch({ type: "AUTH", userdata })
+        navigate("/admin")
+      }
+    })
     .catch(err => console.log(err))
   }
 
   return (
-    <form className='auth-form' onSubmit={handleUSerLogin}>
-      <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type='submit'>Login</button>
+    <form className='auth-form' onSubmit={hanldeUserLogin}>
+      <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value) } />
+      <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value) } />
+      <button type="submit">Login</button>
     </form>
   )
 }

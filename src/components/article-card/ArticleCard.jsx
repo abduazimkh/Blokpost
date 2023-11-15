9fbc1e8d154f0cf4edc0403493e68ca5112ddd13
abduazimkh <azimjon8253@gmail.com>
2023-11-15 10:56:09 +0500

    article card image bag fixed

--- src/components/article-card/ArticleCard.jsx
+++ src/components/article-card/ArticleCard.jsx
@@ -1,18 +1,24 @@
 import "./ArticleCard.scss";
 import { truncate } from "../../helpers/truncate";
 import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { removeTags } from "../../helpers/hooks/remove-tags";
 
 const ArticleCard = ({ id, image, title, description, author, createdAt, category}) => {
-
+  const [errorPlaceholder, setErrorPlaceholder] = useState(false)
   return (
     <div  className='article-card'>
       <div className='article__card-image'>
         <Link to={`/article/${id}`}>
-          <img src={image ? image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSneJHp1-rNCNwaaiGE_U76o3pn8u9EN6NuqRQrSsUlkkA4WKFzuoLgMf1OrjibOsFfDsU&usqp=CAU" } alt="" />
+          <img onError={(e, s) => {
+            if(!s){
+              setErrorPlaceholder(true)
+            }
+          }} src={!image || errorPlaceholder ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSneJHp1-rNCNwaaiGE_U76o3pn8u9EN6NuqRQrSsUlkkA4WKFzuoLgMf1OrjibOsFfDsU&usqp=CAU" : image } alt="" />
         </Link>
       </div>
       <h3>{truncate(title, 35, "...")}</h3>
-      <p>{truncate(description, 50, "...")}</p>
+      <p>{truncate(removeTags(description), 50, "...")}</p>
       <div>
         <p>{author}</p>
         <p>{createdAt}</p>

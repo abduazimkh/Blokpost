d49bf380cbf5b9fad767637cdabb0ab4c24c97b3
abduazimkh <azimjon8253@gmail.com>
2023-11-15 08:47:45 +0500

    reel sceleton

--- src/components/reel/Reel.jsx
+++ src/components/reel/Reel.jsx
@@ -1,46 +1,51 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "./Reel.scss";
 import instance from "../../services/api";
-import { Container } from '../../utils';
-import ArticleCard from '../article-card/ArticleCard';
-
+import { CardSkeleton, Container } from "../../utils";
+import ArticleCard from "../article-card/ArticleCard";
 
 const Reel = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    instance("/api/posts")
+      .then((response) => {
+        setData(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        instance("/api/posts")
-            .then(response => {
-                setData(response.data.data)
-            })
-            .catch(error => console.log(error))
-    }, [])
-
-    console.log(data);
+  console.log(data);
 
   return (
-    <div className='home__reel'>
-        <Container>
-            <h2>Today's trending articles</h2>
-            <div className="reel__wrapper">
-                {
-                    data.map(article =>
-                        <ArticleCard 
-                            id={article._id}
-                            image={article.image}
-                            title={article.title}
-                            description={article.description}
-                            author={article.author}
-                            createdAt={article.createdAt}
-                            category={article.category}
-                        />
-                    )
-                }
-            </div>
-        </Container>
+    <div className="home__reel">
+      <Container>
+        <h2>Today's trending articles</h2>
+        <div className="reel__wrapper">
+          {!loading ? (
+            data.map((article) => (
+              <ArticleCard
+                id={article._id}
+                image={article.image}
+                title={article.title}
+                description={article.description}
+                author={article.author}
+                createdAt={article.createdAt}
+                category={article.category}
+              />
+            ))
+          ) : (
+            <CardSkeleton amount={10} />
+          )}
+        </div>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Reel
+export default Reel;

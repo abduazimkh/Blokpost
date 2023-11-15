0d5f3ce2ea244b0fc95b47d41448eb4e5e4eb409
abduazimkh <azimjon8253@gmail.com>
2023-11-15 08:47:20 +0500

    article sceleton

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -2,30 +2,39 @@ import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import instance from '../../services/api';
-import { Container } from '../../utils';
+import { Container, SingleCardSkeleton } from '../../utils';
 import "./Article.scss";
 
 const Article = () => {
   const { id } = useParams();
   const [data, setData] = useState([]);
-  
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     instance(`/api/posts/${id}`)
-    .then(res => setData(res.data))
+    .then(res => {
+      setData(res.data)
+      setLoading(false)
+    })
+    // .chetch((err) => {
+    //   console.log(err);
+    //   setLoading(false)
+    // })
   }, [])
 
   console.log(data.title);
 
   return (
     <Container>
-      {
+      {!loading ?
         <div className='single-article'>
           <h2>{data.title}</h2>
           <img src={data.image} alt="picture" />
           <p>{data.title}</p>
 
           <p>{data.description}</p>
-        </div>
+        </div> :
+        <SingleCardSkeleton amount={10} />
       }
     </Container>
   )

b31fc6ab9fe4584590f816239f7c285f1682f9cc
abduazimkh <azimjon8253@gmail.com>
2023-11-15 08:45:24 +0500

    stle added

--- src/routes/sub-routes/create/Create.jsx
+++ src/routes/sub-routes/create/Create.jsx
@@ -1,21 +1,23 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
-
+import "./Create.scss";
+import { Button } from "../../../utils";
 
 const Create = () => {
   const [value, setValue] = useState(''); 
   return (
-    <div>
-      <form >
-        <input type="text" placeholder='Title'/>
+    <div className='create'>
+      <form className='create__form'>
+        <input className='form__input' type="text" placeholder='Title'/>
         <ReactQuill theme="snow" value={value} onChange={setValue} />
 
-        <select name="" id="">
+        <select name="" id="" className='form__input'>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
         </select>
+        <Button text={"Create"} />
       </form>
     </div>
   )
